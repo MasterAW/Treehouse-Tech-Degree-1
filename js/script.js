c5a360790509e75dc5ef8c07dcb916a72b07900d
Technabyte <artymail1@gmail.com>
2019-07-19 11:44:31 +0100

    Final commit

--- js/script.js
+++ js/script.js
@@ -9,76 +9,76 @@ var quotes = [
     quote: "The greatest glory in living lies not in never falling, but in rising every time we fall",
     source: "Nelson Mandela",
     citation: "",
-    year: ""
+    year: "",
+    tag: "Motivation"
   },
   {
     quote: "Always be yourself, express yourself, have faith in yourself, do not go out and look for a successful personality and duplicate it.",
     source: "Bruce Lee",
     citation: "",
-    year: ""
+    year: "",
+    tag: "Motivation"
   },
   {
     quote: "The way to get started is to quit talking and begin doing.",
     source: "Walt Disney",
     citation: "",
-    year: ""
+    year: "",
+    tag: "Motivation"
   },
   {
     quote: "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough.",
     source: "Oprah Winfrey",
     citation: "",
-    year: ""
+    year: "",
+    tag: "Life"
   },
   {
     quote: "It's the possibility of having a dream come true that makes life interesting.",
     source: "Paulo Coelho",
     citation: "The Alchemist",
-    year: "1988"
+    year: "1988",
+    tag: "Dream, Life"
   }
 ];
 
 
-// a function that generates a number between 0 and 4 use for array index.
+// a function that generates a number between 0 and 4 use for array index and returns that object.
 function getRandomQuote(){
   var ranNumber = Math.floor( Math.random() * 5 ) ;
-  return ranNumber;
+  var quoteNumber = ranNumber;
+  var ranObject = quotes[quoteNumber];
+  return ranObject;
 
 }
 
 //function that calls getRandomQuote(), checks if citation and year exists and then output the quote with html
 function printQuote(){
-  var quoteNumber = getRandomQuote();
-  var quoteID = quotes[quoteNumber];
+  var quoteObject = getRandomQuote();
   var citation;
   var year;
-  if(quoteID.citation !== "") {
-    citation = "<span class='citation'>" + quoteID.citation + "</span>";
+  if(quoteObject.citation !== "") {
+    citation = "<span class='citation'>" + quoteObject.citation + "</span>";
   }
   else {
     citation = "";
   }
-  if(quoteID.year !== ""){
-    year = "<span class='year'>" + quoteID.year + "</span>";
+  if(quoteObject.year !== ""){
+    year = "<span class='year'>" + quoteObject.year + "</span>";
   }
   else {
     year = "";
   }
-  var output = "<p class='quote'>" + quoteID.quote + "</p><p class='source'>" + quoteID.source + citation + year + "<p>";
+  var string = "<p class='quote'>" + quoteObject.quote + "</p><p class='source'>" + quoteObject.source + citation + year + "<p>" + "<p>Tags: " + quoteObject.tag + "</p>";
+  var output = document.getElementById("quote-box").innerHTML = string;
   return output;
 }
-//returns and output quote
-document.getElementById("quote-box").innerHTML = printQuote();
 
-/***
-  When the "Show another quote" button is clicked, the event listener
-  below will be triggered, and it will call, or "invoke", the `printQuote`
-  function. So do not make any changes to the line of code below this
-  comment.
-***/
-
+//change quote and color when button clicked
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+document.getElementById('loadQuote').addEventListener("click", colorChange, false);
 
-
+//function that uses generates random numbers for red, green, blue
 function randomBackgroundColor(){
   var red = Math.floor( Math.random() * 256) ;
   var green = Math.floor( Math.random() * 256 );
@@ -86,7 +86,15 @@ function randomBackgroundColor(){
   var background = "rgb(" + red + "," + green + "," + blue + ")";
   return background;
 }
+//calls randomBackgroundColor() function and applies it to the body and loadquote id
+function colorChange(){
 document.body.style.backgroundColor = randomBackgroundColor();
 document.getElementById("loadQuote").style.backgroundColor = randomBackgroundColor();
+}
 
-setInterval(printQuote(), 10);
+
+//quote and background color changes every 5 seconds
+var setInterval = setInterval(function(){
+  printQuote();
+  colorChange();
+}, 5000);
